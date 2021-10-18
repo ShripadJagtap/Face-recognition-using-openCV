102e2fd8aaae2f2e929bfb5a5b5568b266292f07
Shripad Jagtap <70714209+ShripadJagtap@users.noreply.github.com>
2021-10-18 22:05:22 +0530

    Updated

--- Basics.py
+++ Basics.py
@@ -5,4 +5,7 @@ import face_recognition
 imgElon = face_recognition.load_image_file('pythonProject/ImagesBasic/Avi.jpg')
 imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)
 imgTest = face_recognition.load_image_file('pythonProject/ImagesBasic/Buuger.jpg')
-imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB) 
+
+faceLoc = face_recognition.face_locations(imgElon)[0]
+encodeElon = face_recognition.face_encodings(imgElon)[0]
