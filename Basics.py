1d22d7f8954c0898b2f33da70f7e0dd9a77db329
Shripad Jagtap <70714209+ShripadJagtap@users.noreply.github.com>
2021-10-18 22:07:03 +0530

    Quick progressions to code
    Co-Authored-By: Phantonym21 <71942501+Phantonym21@users.noreply.github.com>

--- Basics.py
+++ Basics.py
@@ -9,3 +9,4 @@ imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
 faceLoc = face_recognition.face_locations(imgElon)[0]
 encodeElon = face_recognition.face_encodings(imgElon)[0]
+cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255),2)
