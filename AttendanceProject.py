3d05d493997afbe4d086e47afacf90a863c9730b
Shripad Jagtap <70714209+ShripadJagtap@users.noreply.github.com>
2021-10-19 21:19:55 +0530

    Update AttendanceProject.py

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -7,3 +7,9 @@ path = 'pythonProject/ImagesAttendance'
 images = []
 classNames = []
 myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
