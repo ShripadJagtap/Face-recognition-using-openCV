715fd88a8705d71b2a97cf51a1dad5b3a55a116e
Shripad Jagtap <70714209+ShripadJagtap@users.noreply.github.com>
2021-10-20 10:10:26 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -14,3 +14,6 @@ cv2.rectangle(imgElon,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255,0,255
 faceLocTest = face_recognition.face_locations(imgTest)[0]
 encodeTest = face_recognition.face_encodings(imgTest)[0]
 cv2.rectangle(imgTest,(faceLocTest[3],faceLocTest[0]),(faceLocTest[1],faceLocTest[2]),(255,0,255),2)
+
+results = face_recognition.compare_faces([encodeElon],encodeTest)
+faceDis = face_recognition.face_distance([encodeElon],encodeTest)
