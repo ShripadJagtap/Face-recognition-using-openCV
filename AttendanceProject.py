482e6959cc7b1fcf260378cc8fa0532f6024ff0a
Shripad Jagtap <70714209+ShripadJagtap@users.noreply.github.com>
2021-10-21 21:10:30 +0530

    Update AttendanceProject.py

--- AttendanceProject.py
+++ AttendanceProject.py
@@ -39,3 +39,17 @@ while True:
         faceDis = face_recognition.face_distance(encodeListKnown,encodeFace)
         print(faceDis)
         matchIndex = np.argmin(faceDis)
+
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+
+
+            y1,x2,y2,x1 = faceLoc
+            y1, x2, y2, x1 = y1*4,x2*4,y2*4,x1*4
+            cv2.rectangle(img,(x1,y1),(x2,y2),(0,255,0),2)
+            cv2.rectangle(img,(x1,y2-35),(x2,y2),(0,255,0),cv2.FILLED)
+            cv2.putText(img,name,(x1+6,y2-6),cv2.FONT_HERSHEY_COMPLEX,1,(255,255,255),2)
+
+        cv2.imshow('Webcam', img)
+        cv2.waitKey(1)
